8129a819caada70107b77db36474ea16d27bff29
MCXIV <mcxiv14@gmail.com>
2020-10-26 17:18:21 +0530

    Health

---
+++ Assets/Scripts/Player/Damage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Damage : MonoBehaviour
+{
+    public float spikesDamage = 10;
+
+    private LayerMask _spikes;
+    private PlayerData _data;
+    private Rigidbody2D _rb;
+
+    void Start()
+    {
+        
+        _spikes = LayerMask.GetMask("Spikes");
+        _data = GetComponent<PlayerData>();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Physics2D.Raycast(_rb.position, Vector2.down, 2f, _spikes))
+            _data.health -= spikesDamage;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+
+    public float health = 100f;
+    
+}

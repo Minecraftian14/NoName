95ebfe3b577bdf0efd6ab96fa5a4e71e2e9e31c1
MCXIV <mcxiv14@gmail.com>
2020-10-26 23:30:30 +0530

    CamandPound

--- Assets/Scripts/BetterJump.cs
+++ Assets/Scripts/BetterJump.cs
@@ -7,21 +7,42 @@ public class BetterJump : MonoBehaviour
 {
     public int fallSpeed = 2000;
     public int elevator = 400;
-    public Rigidbody2D rb;
+    public float poundforce = 10f;
+
+    private Rigidbody2D _rb;
+    private CameraMovement _cameraMovement;
+    private LayerMask _platformLayer;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _cameraMovement = Camera.main.GetComponent<CameraMovement>();
+        _platformLayer = LayerMask.GetMask("Platform");
     }
 
-    void Update()
+    private float _buffer;
+    private bool _isPound = false;
+
+    void FixedUpdate()
     {
-        if (rb.velocity.y < 0)
+        if (_rb.velocity.y < 0)
         {
-            if (Input.GetKeyDown(KeyCode.S))
-                rb.AddForce(Time.deltaTime * fallSpeed * elevator * Vector2.down);
+            if (!_isPound && Input.GetKeyDown(KeyCode.S))
+            {
+                _rb.AddForce(Time.deltaTime * fallSpeed * elevator * Vector2.down);
+                _buffer = _cameraMovement.modifier;
+                _cameraMovement.modifier = 1;
+                _isPound = true;
+            }
             else
-                rb.AddForce(Time.deltaTime * fallSpeed * Vector2.down);
+                _rb.AddForce(Time.deltaTime * fallSpeed * Vector2.down);
+        }
+
+        print(_buffer);
+        if (_isPound && Physics2D.Raycast(_rb.position, Vector2.down, 2f, _platformLayer))
+        {
+            _cameraMovement.modifier = _buffer;
+            _isPound = false;
         }
     }
 }
---
+++ Assets/Scripts/GroundPound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class GroundPound : MonoBehaviour
+{
+    public float poundforce = 10f;
+    private Rigidbody2D _rb;
+    private CameraMovement _cameraMovement;
+    private LayerMask _platformLayer;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _cameraMovement = Camera.main.GetComponent<CameraMovement>();
+        _platformLayer = LayerMask.GetMask("Platform");
+    }
+
+    private float _buffer;
+    private bool _isPound = false;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.S))
+        {
+            _rb.AddForce(Time.deltaTime * poundforce * Vector2.down);
+            _buffer = _cameraMovement.modifier;
+            _cameraMovement.modifier = 1;
+            _isPound = true;
+        }
+        else if (_isPound && Physics2D.Raycast(_rb.position, Vector2.down, 2f, _platformLayer))
+        {
+            _cameraMovement.modifier = _buffer;
+            _isPound = false;
+        }
+    }
+}

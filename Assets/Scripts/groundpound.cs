5e81ad27a75f283676e909669be53ca4b00d1230
Dis-is-Sid <rbrijkaushly@gmail.com>
2020-10-26 22:58:49 +0530

    groundpond

---
+++ Assets/Scripts/groundpound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundpound : MonoBehaviour
+{
+    public float poundforce = 10f;
+    private Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.S))
+		{
+            _rb.AddForce(Vector2.down * poundforce * Time.deltaTime);
+		}
+    }
+}

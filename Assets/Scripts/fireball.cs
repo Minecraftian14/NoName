e34c574cb68809213fb4c4a9af6456b56da4765f
Dis-is-Sid <rbrijkaushly@gmail.com>
2020-10-27 21:54:32 +0530

    firebbball

---
+++ Assets/Scripts/fireball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireball : MonoBehaviour
+{
+
+    private Rigidbody2D _rb;
+    public GameObject fired;
+    public           Transform firepoinnt;
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_rb.velocity.x !=0)
+        transform.rotation = Quaternion.Euler(0, _rb.velocity.x < 0 ? 180 : 0, 0);
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(fired,firepoinnt.position,firepoinnt.rotation);
+            
+        }
+    }
+}
---
+++ Assets/fireballdestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireballdestroy : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    
+        void OnTriggerEnter2D()
+		{
+            Destroy (gameObject);
+		}
+    
+}
